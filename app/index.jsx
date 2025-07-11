a673dd0e1bd3f92f86c48c1ed6041872667e9233
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2025-07-11 23:56:40 +0200

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,39 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+import logo from "../assets/img/logo_light.png";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.image} />
+      <Text style={styles.title}>The number 1</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+
+      <View style={styles.card}>
+        <Text>Hello, this is a card</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  card: {
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow: "4px 4px rgba(0,0,0,0.1)",
+  },
+  image: {
+    marginVertical: 20,
+  },
+});

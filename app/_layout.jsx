cd9e6d5b8ddf6cf7db8cdd90617177e72119b7ab
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2025-07-12 23:34:50 +0200

    Cange main view component and fix status bar to back color background

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,22 +1,30 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, useColorScheme, View } from "react-native";
 import { Stack } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Colors } from "../constants/colors";
+import { StatusBar } from "expo-status-bar";
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme] ?? Colors.light;
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Stack
-        screenOptions={{
-          animation: "flip",
-          headerStyle: { backgroundColor: "#ddd" },
-          headerTintColor: "#333",
-        }}
-      >
-        <Stack.Screen name="index" options={{ title: "Home" }} />
-        <Stack.Screen name="about" options={{ title: "About us" }} />
-        <Stack.Screen name="contact" options={{ title: "Contact us" }} />
-      </Stack>
-    </SafeAreaView>
+    <>
+      <StatusBar style="auto" />
+      <View style={{ flex: 1 }}>
+        <Stack
+          screenOptions={{
+            animation: "flip",
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title,
+          }}
+        >
+          <Stack.Screen name="index" options={{ title: "Home" }} />
+          <Stack.Screen name="about" options={{ title: "About us" }} />
+          <Stack.Screen name="contact" options={{ title: "Contact us" }} />
+        </Stack>
+      </View>
+    </>
   );
 };
 

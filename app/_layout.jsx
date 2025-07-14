b212d3fc2807bff5a7c5f292a6bf753676697483
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2025-07-14 21:23:31 +0200

    remove non-excisting pages from layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -19,8 +19,6 @@ const RootLayout = () => {
           }}
         >
           <Stack.Screen name="index" options={{ title: "Home" }} />
-          <Stack.Screen name="about" options={{ title: "About us" }} />
-          <Stack.Screen name="contact" options={{ title: "Contact us" }} />
         </Stack>
       </View>
     </>

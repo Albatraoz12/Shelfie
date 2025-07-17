5c3ff75e69854bd16cd21f57491d46ae54b79f81
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2025-07-17 16:56:48 +0200

    create a pressable compont with dummy function

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,18 +1,30 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Pressable, Text } from "react-native";
 import { Link } from "expo-router";
 
 // Themed components
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
+import { Colors } from "../../constants/Colors";
 
 export default function Login() {
+  const handleSubmit = () => {
+    console.log("Login form submitted");
+  };
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title={true} style={styles.title}>
         Login to your account
       </ThemedText>
+
+      <Pressable
+        onPress={handleSubmit}
+        style={({ pressed }) => [styles.btn, pressed && styles.pressed]}
+      >
+        <Text style={{ color: "#f2f2f2" }}>Login</Text>
+      </Pressable>
 
       <Spacer height={100} />
       <Link href="/register">
@@ -33,5 +45,13 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30,
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
   },
 });

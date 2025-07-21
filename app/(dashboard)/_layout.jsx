0d6c247becc3761544791545e4308416a2992e42
Dimosthenis Emmanouil <Dimosthenis.emmanouil@gmail.com>
2025-07-21 21:17:59 +0200

    add styling to tabs navigation

--- app/(dashboard)/_layout.jsx
+++ app/(dashboard)/_layout.jsx
@@ -6,7 +6,20 @@ const DashboardLayouts = () => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
 
-  return <Tabs screenOptions={{ headerShown: false }} />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: theme.navBackground,
+          padding: 10,
+          height: 90,
+        },
+        tabBarActiveTintColor: theme.iconColorFocused,
+        tabBarInactiveTintColor: theme.iconColor,
+      }}
+    />
+  );
 };
 
 export default DashboardLayouts;
